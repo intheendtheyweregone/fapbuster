3d676de729b7cc9c2098f4eabeba529eb6a25dfb
End <114883905+intheendtheyweregone@users.noreply.github.com>
2022-10-15 20:16:38 -0600

    house keeping

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const client = require('https');
 const { exit, argv } = require('process');
 
 const arguments = argv.slice(2);
+if (!arguments[0] || !arguments[1]) return console.log('Missing Argument')
+if (isNaN(arguments[1])) return console.log('The second argument must be a number.')
+
+let model_name = arguments[0]
+// get both parts of models name
+let first = model_name.slice(0, 1)
+let second = model_name.slice(1, 2)
+
+let num = arguments[1]++;
+    
 
 function downloadImage(url, filepath) {
     return new Promise((resolve, reject) => {
@@ -14,51 +24,49 @@ function downloadImage(url, filepath) {
                     .on('error', reject)
                     .once('close', () => resolve(filepath));
             } else {
-                // Consume response data to free up memory
                 res.resume();
-                reject(new Error(`Request Failed With a Status Code: ${res.statusCode}`));
-                
+                reject(new Error(`Request got 404 - Out of content?`));
+
             }
         });
     });
 }
 
 
-let model_name = arguments[0]
 function addLeadingZeros(num, totalLength) {
     return String(num).padStart(totalLength, '0');
-  }
+}
 
-  var dir = './' + model_name;
+var dir = './' + model_name;
 
-if (!fs.existsSync(dir)){
+if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
 }
-  
 
-let first = model_name.slice(0, 1)
-let second = model_name.slice(1,2)
+
+
 
 const sleep = (time) => {
     return new Promise((resolve) => setTimeout(resolve, time))
-  }
-  
-	let num = arguments[1]++;
-			console.log(num)	
-    const download = async() => { for(let i = 1; i < num; i++) {
-        if (i > 100) {
-            downloadImage("https://fapello.com/content/"+first+"/"+second+"/"+model_name+"/1000/"+model_name+"_" + addLeadingZeros(i, 4) + ".jpg", model_name + '/' + randomUUID() + '.jpg' )
-            .then(console.log)
-            .catch(console.error);
-            
-        }else{
-            downloadImage("https://fapello.com/content/"+first+"/"+second+"/"+model_name+"/1000/"+model_name+"_" + addLeadingZeros(i, 4) + ".jpg", model_name + '/' + randomUUID() + '.jpg' )
-            .then(console.log)
-            .catch(console.error);
-        }
-        await sleep(350)
+}
 
-        
+
+
+const download = async () => {
+    for (let i = 1; i < num; i++) {
+        if (i > 100) {
+            downloadImage("https://fapello.com/content/" + first + "/" + second + "/" + model_name + "/1000/" + model_name + "_" + addLeadingZeros(i, 4) + ".jpg", model_name + '/' + randomUUID() + '.jpg')
+                .then(console.log)
+                .catch(console.error);
+
+        } else {
+            downloadImage("https://fapello.com/content/" + first + "/" + second + "/" + model_name + "/1000/" + model_name + "_" + addLeadingZeros(i, 4) + ".jpg", model_name + '/' + randomUUID() + '.jpg')
+                .then(console.log)
+                .catch(console.error);
+        }
+        await sleep(365)
+
+
     }
 }
 
